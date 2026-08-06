fea9ee1acefc1d048f7f0c35ffea434bca943f32
dekofit <mr.dendenis23@yandex.ru>
2026-08-06 14:55:41 +0300

    Add new class item

---
+++ src/models/item.py
@@ -0,0 +1,8 @@
+class Item:
+    def __init__(self, id, barcode, weight, width, height, length):
+        self.id = id
+        self.barcode = barcode
+        self.weight = weight
+        self.width = width
+        self.height = height
+        self.length = length
