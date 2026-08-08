b3ed9f51bd29261aa359d1dc5f3c855611a140a7
dekofit <mr.dendenis23@yandex.ru>
2026-08-08 19:27:15 +0300

    Complete Item model implementation

--- src/main.py
+++ src/main.py
@@ -8,9 +8,13 @@ def main():
         width=250,
         height=150,
         length=300,
+        category="Electronics",
+        delivery_type="Courier",
+        is_flammable=False,
+        status="CREATED",
+        destination=None,
+        location="Scanner",
     )
-
-    print(item.__dict__)
 
 if __name__ == "__main__":
     main()
--- src/models/item.py
+++ src/models/item.py
@@ -1,11 +1,39 @@
 class Item:
-    def __init__(self, id, barcode, weight, width, height, length):
+    def __init__(self, id, barcode, weight, width, height, length, category, delivery_type, is_flammable, status, destination, location):
         self.id = id
         self.barcode = barcode
         self.weight = weight
         self.width = width
         self.height = height
         self.length = length
+        self.category = category
+        self.delivery_type = delivery_type
+        self.is_flammable = is_flammable
+        self.status = status
+        self.destination = destination
+        self.location = location
 
-    def __str__(self):
-        pass
+    def get_info(self):
+        return {
+            "id": self.id,
+            "barcode": self.barcode,
+            "weight": self.weight,
+            "width": self.width,
+            "height": self.height,
+            "length": self.length,
+            "category": self.category,
+            "delivery_type": self.delivery_type,
+            "is_flammable": self.is_flammable,
+            "status": self.status,
+            "destination": self.destination,
+            "location": self.location,
+        }
+
+    def change_status(self, new_status):
+        self.status = new_status
+
+    def set_destination(self, destination):
+        self.destination = destination
+
+    def update_location(self, location):
+        self.location = location
